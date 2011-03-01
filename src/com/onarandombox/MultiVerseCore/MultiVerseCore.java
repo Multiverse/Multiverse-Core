187c6acbb629fe5b42eb2b2f2c39d695124b20d6
Protected <myshelter@gmail.com>
2011-03-01 18:05:24 +0000

    Teleporter factory method

--- src/com/onarandombox/MultiVerseCore/MultiVerseCore.java
+++ src/com/onarandombox/MultiVerseCore/MultiVerseCore.java
@@ -232,6 +232,10 @@ public class MultiVerseCore extends JavaPlugin {
             return playerSessions.get(player.getName());
         }
     }
+    
+    public MVTeleport getTeleporter() {
+    	return new MVTeleport(this);
+    }
 	
 	/**
 	 * onCommand
