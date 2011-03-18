09c540453fe219dd6577c7b7223eab53b089e9b5
Rigby <rigby@onarandombox.com>
2011-03-18 11:52:00 +0000

    If you're happy and you know it - make it easier to read. (Boredom)

--- src/com/onarandombox/utils/PurgeWorlds.java
+++ src/com/onarandombox/utils/PurgeWorlds.java
@@ -39,13 +39,20 @@ public class PurgeWorlds {
         int count = 0;
         
         for(Entity e: entities){
-            if ((((e instanceof Creeper)) && (creatures.contains("CREEPER"))) || (((e instanceof Skeleton)) && (creatures.contains("SKELETON"))) || 
-                    (((e instanceof Spider)) && (creatures.contains("SPIDER"))) || (((e instanceof Zombie)) && (creatures.contains("ZOMBIE"))) || 
-                        (((e instanceof Ghast)) && (creatures.contains("GHAST"))) || (((e instanceof PigZombie)) && (creatures.contains("PIGZOMBIE"))) || 
-                            (((e instanceof Giant)) && (creatures.contains("GIANT"))) || (((e instanceof Slime)) && (creatures.contains("SLIME"))) || 
-                                (((e instanceof Chicken)) && (creatures.contains("CHICKEN"))) || (((e instanceof Cow)) && (creatures.contains("COW"))) || 
-                                    (((e instanceof Sheep)) && (creatures.contains("SHEEP"))) || (((e instanceof Pig)) && (creatures.contains("PIG"))) || (
-                                        ((e instanceof Squid)) && (creatures.contains("SQUID"))) || creatures.contains("*")) {
+            if ((((e instanceof Creeper)) && (creatures.contains("CREEPER"))) || 
+                (((e instanceof Skeleton)) && (creatures.contains("SKELETON"))) ||
+                (((e instanceof Spider)) && (creatures.contains("SPIDER"))) || 
+                (((e instanceof Zombie)) && (creatures.contains("ZOMBIE"))) || 
+                (((e instanceof Ghast)) && (creatures.contains("GHAST"))) || 
+                (((e instanceof PigZombie)) && (creatures.contains("PIGZOMBIE"))) || 
+                (((e instanceof Giant)) && (creatures.contains("GIANT"))) || 
+                (((e instanceof Slime)) && (creatures.contains("SLIME"))) || 
+                (((e instanceof Chicken)) && (creatures.contains("CHICKEN"))) || 
+                (((e instanceof Cow)) && (creatures.contains("COW"))) || 
+                (((e instanceof Sheep)) && (creatures.contains("SHEEP"))) || 
+                (((e instanceof Pig)) && (creatures.contains("PIG"))) || 
+                (((e instanceof Squid)) && (creatures.contains("SQUID"))) || 
+                creatures.contains("*")) {
                 e.remove();
                 count++;
             }
