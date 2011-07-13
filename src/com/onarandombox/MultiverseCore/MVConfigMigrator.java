3856d6777c93f8fa4488f688bc7b27d7b973bd85
Eric Stokes <fernferret@gmail.com>
2011-07-12 21:13:56 -0600

    Add new configMigrator class, update allpay

---
+++ src/com/onarandombox/MultiverseCore/MVConfigMigrator.java
@@ -0,0 +1,8 @@
+package com.onarandombox.MultiverseCore;
+
+public class MVConfigMigrator {
+    private MultiverseCore core;
+    public MVConfigMigrator(MultiverseCore core) {
+        this.core = core;
+    }
+}
