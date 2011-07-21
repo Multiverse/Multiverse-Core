04accbfdeda29c31c1dec23f195cb96e1476f4de
Eric Stokes <fernferret@gmail.com>
2011-07-21 17:27:33 -0600

    Add exact destination

--- src/main/java/com/onarandombox/utils/Destination.java
+++ src/main/java/com/onarandombox/utils/Destination.java
@@ -1,29 +1,46 @@
 package com.onarandombox.utils;
 
+import java.util.Arrays;
+import java.util.List;
+
 import com.onarandombox.MultiverseCore.MultiverseCore;
 
 public class Destination {
     private String name;
+    private double x;
+    private double y;
+    private double z;
+    private double pitch;
+    private double yaw;
     private DestinationType type;
 
     public Destination(String name, DestinationType type) {
         this.name = name;
         this.type = type;
-        
-        if(name.split(":").length > 1) {
+
+        if (name.split(":").length > 1) {
             this.type = DestinationType.Invalid;
         }
     }
-    
+
+    public Destination(String coordString) {
+        this.type = DestinationType.Invalid;
+        if(this.setExactDest(coordString.split(":"))) {
+            this.type = DestinationType.Exact;
+        }
+    }
+
     @Override
     public String toString() {
-        if(this.type == DestinationType.Portal) {
-            return "p:" + this.name;            
-        } else if(this.type == DestinationType.World) {
+        if (this.type == DestinationType.Portal) {
+            return "p:" + this.name;
+        } else if (this.type == DestinationType.World) {
             return "w:" + this.name;
+        } else if (this.type == DestinationType.Exact) {
+            return "e:" + this.x + "," + this.y + "," + this.z + ":" + this.pitch + ":" + this.yaw;
         }
         return "i:" + this.name;
-        
+
     }
 
     public String getName() {
@@ -40,8 +57,8 @@ public class Destination {
 
     /**
      * Takes the given string and returns a Destination. This will NEVER be NULL. It will return a destination of type Invalid if the destination is bad.
-     *
-     * @param dest   The parceable string, ex: w:My World
+     * 
+     * @param dest The parceable string, ex: w:My World
      * @param plugin The MultiverseCore plugin used to find valid worlds/portals
      * @return A new Destination from the parsed string.
      */
@@ -57,7 +74,7 @@ public class Destination {
         if (items.length == 1 && items[0].equalsIgnoreCase("here")) {
             return new Destination(items[0], DestinationType.Exact);
         }
-        
+
         // If we only found one param, assume world, but still check for validity
         if (items.length == 1 && plugin.isMVWorld(items[0])) {
             return new Destination(items[0], DestinationType.World);
@@ -68,10 +85,43 @@ public class Destination {
         } else if (items[0].equalsIgnoreCase("p")) {
             // TODO: Check for a valid portal, we can't right now, as portals aren't really in yet.
             return new Destination(items[1], DestinationType.Portal);
-        } else if(items[0].equalsIgnoreCase("e")) {
-            // TODO: The new 'exact' dest type
+        } else if (items[0].equalsIgnoreCase("e")) {
+            return new Destination(dest);
         }
         System.out.print("Nothing valid found!!");
         return getBadDestination();
+    }
+
+    private boolean setExactDest(String[] items) {
+        List<String> parsed = Arrays.asList(items);
+        // we already know this is an 'e'
+        // e:x,y,z:pitch:yaw
+        parsed.remove(0);
+        String[] coordString = parsed.get(0).split(",");
+        Double[] coords = new Double[3];
+        for (int i = 0; i < 3; i++) {
+            try {
+                coords[i] = Double.parseDouble(coordString[0]);
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        }
+        x = coords[0];
+        y = coords[1];
+        z = coords[2];
+        if (parsed.size() == 1) {
+            // We parsed with X, Y, And Z, we're good to go!
+            return true;
+        }
+        if (parsed.size() == 3) {
+            try {
+                this.pitch = Double.parseDouble(parsed.get(1));
+                this.yaw = Double.parseDouble(parsed.get(1));
+            } catch (NumberFormatException e) {
+                return false;
+            }
+            return true;
+        }
+        return false;
     }
 }
