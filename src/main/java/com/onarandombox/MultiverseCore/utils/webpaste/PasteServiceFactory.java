5e7d883c8210d1e161ee348ff441c301019f59fa
Tim Ekl <lithium3141@gmail.com>
2011-11-28 16:11:59 -0500

    Refactor out pastebin service; switch to private pastie by default

--- src/main/java/com/onarandombox/MultiverseCore/utils/webpaste/PasteServiceFactory.java
+++ src/main/java/com/onarandombox/MultiverseCore/utils/webpaste/PasteServiceFactory.java
@@ -3,7 +3,7 @@ package com.onarandombox.MultiverseCore.utils.webpaste;
 public class PasteServiceFactory {
     public static PasteService getService(PasteServiceType type, boolean isPrivate) {
         switch(type) {
-            case PASTEBIN: return null;
+            case PASTEBIN: return new PastebinPasteService(isPrivate);
             case PASTIE: return new PastiePasteService(isPrivate);
             default: return null;
         }
