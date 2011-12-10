ea6b02085029e1b34edf747a6d8c1fc3284dbe67
Eric Stokes <fernferret@gmail.com>
2011-12-10 14:15:18 -0700

    Add test for Failure to select a valid command

--- src/test/java/com/onarandombox/MultiverseCore/test/TestWorldStuff.java
+++ src/test/java/com/onarandombox/MultiverseCore/test/TestWorldStuff.java
@@ -204,6 +204,9 @@ public class TestWorldStuff {
             fail("Mode property did not exist.");
         }
 
+        plugin.onCommand(mockCommandSender, mockCommand, "", new String[]{ "modify", "set", "blah", "fish", "world" });
+        verify(mockCommandSender).sendMessage(ChatColor.RED + "Sorry, You can't set: '"+ChatColor.GRAY+ "blah" + ChatColor.RED + "'");
+
     }
 
     private void createInitialWorlds(Plugin plugin, Command command) {
