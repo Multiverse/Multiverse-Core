2b80ca80ae033fe1be946c4dc4194298080326ed
Jeremy Wood <farachan@gmail.com>
2012-07-25 22:10:06 -0400

    Bumped protocol from 16 to 17.

--- src/main/java/com/onarandombox/MultiverseCore/MultiverseCore.java
+++ src/main/java/com/onarandombox/MultiverseCore/MultiverseCore.java
@@ -104,7 +104,7 @@ import java.util.logging.Logger;
  * The implementation of the Multiverse-{@link Core}.
  */
 public class MultiverseCore extends JavaPlugin implements MVPlugin, Core {
-    private static final int PROTOCOL = 16;
+    private static final int PROTOCOL = 17;
     // TODO: Investigate if this one is really needed to be static.
     // Doubt it. -- FernFerret
     private static Map<String, String> teleportQueue = new HashMap<String, String>();
