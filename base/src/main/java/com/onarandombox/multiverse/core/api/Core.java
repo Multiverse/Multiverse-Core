0395ead29df1f88b71e7c763bc1ac14fec1bd540
Jeremy Wood <farachan@gmail.com>
2012-11-09 12:54:19 -0500

    Clarified comments in Core.

--- base/src/main/java/com/onarandombox/multiverse/core/api/Core.java
+++ base/src/main/java/com/onarandombox/multiverse/core/api/Core.java
@@ -6,7 +6,7 @@ import com.dumptruckman.minecraft.pluginbase.plugin.PluginBase;
  * Multiverse 3 Core API
  * <p>
  * This API contains a bunch of useful things you can get out of Multiverse in general!
- * This is the class you should cast your plugin to.
+ * This is the class you should cast your plugin to unless you need more Implementation specific API.
  */
 public interface Core extends PluginBase<CoreConfig> {
 
