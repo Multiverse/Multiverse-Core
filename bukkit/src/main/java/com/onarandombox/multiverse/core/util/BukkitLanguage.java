5ed096fb8f9b3e7255351b815425cd07bc793241
Jeremy Wood <farachan@gmail.com>
2012-11-09 12:49:22 -0500

    Added a Language class for Bukkit specific language.

---
+++ bukkit/src/main/java/com/onarandombox/multiverse/core/util/BukkitLanguage.java
@@ -0,0 +1,9 @@
+package com.onarandombox.multiverse.core.util;
+
+import com.dumptruckman.minecraft.pluginbase.locale.Message;
+
+public class BukkitLanguage {
+
+    public static final Message CREATE_WORLD_ERROR = new Message("worlds.create_world_error",
+            "&aBukkit&f experienced a problem while attempting to create '&b%s&f'!");
+}
