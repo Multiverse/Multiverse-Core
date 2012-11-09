2a56398d10394ac892b7e7ec901e9ef46daabdd4
main() <main@maincraft.tk>
2012-11-09 21:17:08 +0100

    TABS!? What are you doing, Eclipse!?

--- api/src/main/java/com/mvplugin/features/PrefixChatFeature.java
+++ api/src/main/java/com/mvplugin/features/PrefixChatFeature.java
@@ -1,6 +1,12 @@
 package com.mvplugin.features;
 
+/**
+ * This is the prefixChat-feature.
+ * <p>
+ * It has a boolean state. If that state is {@code true}, all chat messages will be prefixed with the
+ * name of the world the sender is in, if the state is {@code true}, nothing will happen.
+ */
 public interface PrefixChatFeature {
-	boolean isPrefixChatEnabled();
-	void setPrefixChatState(boolean enabled);
+    boolean isPrefixChatEnabled();
+    void setPrefixChatState(boolean enabled);
 }
