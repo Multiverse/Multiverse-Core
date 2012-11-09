ea38d3dc4e0d83ea7a04154966751c2e69ea4320
Jeremy Wood <farachan@gmail.com>
2012-11-09 12:56:29 -0500

    BukkitWorldManager should now be able to create a BukkitMultiverseWorld.

--- bukkit/src/main/java/com/onarandombox/multiverse/core/BukkitWorldManager.java
+++ bukkit/src/main/java/com/onarandombox/multiverse/core/BukkitWorldManager.java
@@ -1,26 +1,43 @@
 package com.onarandombox.multiverse.core;
 
+import com.dumptruckman.minecraft.pluginbase.locale.BundledMessage;
 import com.dumptruckman.minecraft.pluginbase.util.Logging;
-import com.onarandombox.multiverse.core.api.Core;
-import com.onarandombox.multiverse.core.api.MultiverseWorld;
+import com.onarandombox.multiverse.core.api.BukkitMultiverseWorld;
+import com.onarandombox.multiverse.core.api.WorldProperties;
 import com.onarandombox.multiverse.core.minecraft.WorldEnvironment;
 import com.onarandombox.multiverse.core.minecraft.WorldType;
+import com.onarandombox.multiverse.core.util.BukkitLanguage;
 import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.World.Environment;
 import org.bukkit.WorldCreator;
 
-class BukkitWorldManager extends AbstractWorldManager {
+import java.io.File;
+import java.io.IOException;
 
-    BukkitWorldManager(final Core core) {
+public class BukkitWorldManager extends AbstractWorldManager<BukkitMultiverseWorld> {
+
+    private final MultiverseCore plugin;
+    private final File worldsFolder;
+
+    BukkitWorldManager(final MultiverseCore core) {
         super(core);
+        this.plugin = core;
+        worldsFolder = new File(core.getDataFolder(), "worlds");
     }
 
     @Override
-    public MultiverseWorld createWorld(String name, WorldEnvironment env, Long seed, WorldType type, Boolean generateStructures, String generator, boolean useSpawnAdjust) throws WorldCreationException {
+    public WorldProperties getWorldProperties(final String worldName) throws IOException {
+        return new YamlWorldProperties(plugin, new File(worldsFolder, worldName + ".yml"));
+    }
+
+    @Override
+    public BukkitMultiverseWorld createWorld(String name, WorldEnvironment env, Long seed, WorldType type,
+                                             Boolean generateStructures, String generator) throws WorldCreationException {
         if (Bukkit.getWorld(name) != null) {
-            
+            return null;
         }
-        WorldCreator c = new WorldCreator(name);
+        final WorldCreator c = new WorldCreator(name);
         if (seed != null) {
             c.seed(seed);
         }
@@ -46,5 +63,11 @@ class BukkitWorldManager extends AbstractWorldManager {
             builder.append(" & generator: ").append(generator);
         }
         Logging.info(builder.toString());
+        try {
+            final World world = c.createWorld();
+            return new BukkitWorld(world, getWorldProperties(name));
+        } catch (Exception e) {
+            throw new WorldCreationException(new BundledMessage(BukkitLanguage.CREATE_WORLD_ERROR, name), e);
+        }
     }
 }
