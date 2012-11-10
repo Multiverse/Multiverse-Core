020dee2ab0e5966b6ed1cd0bd8d91033f3a1041e
Jeremy Wood <farachan@gmail.com>
2012-11-10 10:55:22 -0500

    Exception messages now formatted better.

--- base/src/main/java/com/mvplugin/core/MultiverseException.java
+++ base/src/main/java/com/mvplugin/core/MultiverseException.java
@@ -1,20 +1,21 @@
 package com.mvplugin.core;
 
 import com.dumptruckman.minecraft.pluginbase.messaging.BundledMessage;
+import com.dumptruckman.minecraft.pluginbase.messaging.ChatColor;
 
 public class MultiverseException extends Exception {
 
     private final BundledMessage languageMessage;
 
 
-    public MultiverseException(BundledMessage languageMessage) {
-        super(languageMessage.getMessage().getDefault().get(0));
-        this.languageMessage = languageMessage;
+    public MultiverseException(BundledMessage b) {
+        super(String.format(ChatColor.translateAlternateColorCodes('&', b.getMessage().getDefault().get(0)), b.getArgs()));
+        this.languageMessage = b;
     }
 
-    public MultiverseException(BundledMessage languageMessage, Throwable throwable) {
-        super(languageMessage.getMessage().getDefault().get(0), throwable);
-        this.languageMessage = languageMessage;
+    public MultiverseException(BundledMessage b, Throwable throwable) {
+        super(String.format(ChatColor.translateAlternateColorCodes('&', b.getMessage().getDefault().get(0)), b.getArgs()), throwable);
+        this.languageMessage = b;
     }
 
     public BundledMessage getBundledMessage() {
