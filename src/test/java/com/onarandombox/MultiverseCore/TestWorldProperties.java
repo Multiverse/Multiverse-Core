f3a420d85cc0a96cfc58c3ea5cfaca8ab153eb9b
Eric Stokes <fernferret@gmail.com>
2016-11-19 14:18:12 -0700

    Added additional properties to the test checks for world properties

--- src/test/java/com/onarandombox/MultiverseCore/TestWorldProperties.java
+++ src/test/java/com/onarandombox/MultiverseCore/TestWorldProperties.java
@@ -19,6 +19,7 @@ import org.bukkit.Difficulty;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.WorldType;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -106,6 +107,9 @@ public class TestWorldProperties {
 
         assertEquals(core.getServer().getWorlds().size(), 1);
         assertEquals(core.getServer().getWorlds().get(0).getName(), "world");
+        assertEquals(core.getServer().getWorlds().get(0).getEnvironment(), World.Environment.NORMAL);
+        assertEquals(core.getServer().getWorlds().get(0).getWorldType(), WorldType.NORMAL);
+
 
         // Import a second world
         String[] netherArgs = new String[] { "import", "world_nether", "nether" };
@@ -117,6 +121,8 @@ public class TestWorldProperties {
         assertEquals(core.getServer().getWorlds().size(), 2);
         assertEquals(core.getServer().getWorlds().get(0).getName(), "world");
         assertEquals(core.getServer().getWorlds().get(1).getName(), "world_nether");
+        assertEquals(core.getServer().getWorlds().get(1).getEnvironment(), World.Environment.NETHER);
+        assertEquals(core.getServer().getWorlds().get(1).getWorldType(), WorldType.NORMAL);
 
         // ////////////////////////////////////////////////
         // let's set some world-properties
