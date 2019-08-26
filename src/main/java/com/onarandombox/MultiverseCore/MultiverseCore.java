22ad7214b09dc11a53f61941cf40340ec9a4eb20
Jeremy Wood <jwood@aquent.com>
2019-08-26 13:47:26 -0400

    Made the donation messages at startup hidden with silent start.

--- src/main/java/com/onarandombox/MultiverseCore/MultiverseCore.java
+++ src/main/java/com/onarandombox/MultiverseCore/MultiverseCore.java
@@ -340,8 +340,8 @@ public class MultiverseCore extends JavaPlugin implements MVPlugin, Core {
         // Output a little snippet to show it's enabled.
         Logging.config("Version %s (API v%s) Enabled - By %s", this.getDescription().getVersion(), PROTOCOL, getAuthors());
 
-        getLogger().info("Help dumptruckman keep this project alive. Become a patron! https://www.patreon.com/dumptruckman");
-        getLogger().info("One time donations are also appreciated: https://www.paypal.me/dumptruckman");
+        getLogger().config("Help dumptruckman keep this project alive. Become a patron! https://www.patreon.com/dumptruckman");
+        getLogger().config("One time donations are also appreciated: https://www.paypal.me/dumptruckman");
     }
 
     /**
