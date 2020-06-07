e821611744208ec90a787ed380e8428c8a5632d7
Kermina Awad <kerminaawad@gmail.com>
2020-06-06 20:05:33 -0400

    fix pasting to hastebin

--- src/main/java/com/onarandombox/MultiverseCore/utils/webpaste/HastebinPasteService.java
+++ src/main/java/com/onarandombox/MultiverseCore/utils/webpaste/HastebinPasteService.java
@@ -9,6 +9,7 @@ import java.io.OutputStreamWriter;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 /**
@@ -43,14 +44,19 @@ public class HastebinPasteService implements PasteService {
             URLConnection conn = url.openConnection();
             conn.setDoOutput(true);
 
-            wr = new OutputStreamWriter(conn.getOutputStream());
-            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            // hastebin needs a user-agent
+            conn.addRequestProperty("User-Agent", "placeholder");
+            // this isn't required, but is technically correct
+            conn.addRequestProperty("Content-Type", "text/plain; charset=utf-8");
 
+            wr = new OutputStreamWriter(conn.getOutputStream(), StandardCharsets.UTF_8.newEncoder());
             wr.write(encodedData);
             wr.flush();
 
             String line;
             StringBuilder responseString = new StringBuilder();
+            // this has to be initialized AFTER the data has been flushed!
+            rd = new BufferedReader(new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8));
             while ((line = rd.readLine()) != null) {
                 responseString.append(line);
             }
