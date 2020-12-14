b34c93fc24d7b67627dc1589a08f83c8a7428d30
benwoo1110 <30431861+benwoo1110@users.noreply.github.com>
2020-12-14 20:21:47 +0800

    Update mapInitialize to use non deprecated methods.

--- src/main/java/com/onarandombox/MultiverseCore/listeners/MVMapListener.java
+++ src/main/java/com/onarandombox/MultiverseCore/listeners/MVMapListener.java
@@ -1,5 +1,6 @@
 package com.onarandombox.MultiverseCore.listeners;
 
+import com.dumptruckman.minecraft.util.Logging;
 import com.onarandombox.MultiverseCore.MultiverseCore;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
@@ -8,6 +9,9 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.server.MapInitializeEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.MapMeta;
 import org.bukkit.map.MapView;
 
 /**
@@ -23,21 +27,44 @@ public class MVMapListener implements Listener {
 
     /**
      * This method is called when a map is initialized.
+     * We need this to set the correct player location on map.
+     *
      * @param event The event that was fired.
      */
     @EventHandler
     public void mapInitialize(final MapInitializeEvent event) {
-        for (final Player player : Bukkit.getOnlinePlayers()) {
-            if ((player.getItemInHand().getType() == Material.MAP
-                    || player.getItemInHand().getType() == Material.FILLED_MAP)
-                    && player.getItemInHand().getDurability() == event.getMap().getId()) {
-                final Location playerLoc = player.getLocation();
-                final MapView map = event.getMap();
-                map.setCenterX(playerLoc.getBlockX());
-                map.setCenterZ(playerLoc.getBlockZ());
-                map.setWorld(playerLoc.getWorld());
-                return;
-            }
+        final MapView map = event.getMap();
+        final Player player = getPlayerHoldingMap(map);
+        if (player == null) {
+            Logging.fine("No player found with holding map %s.", map.getId());
+            return;
         }
+
+        Logging.fine("Setting map %s location that %s is holding.", map.getId(), player.getName());
+        Location playerLoc = player.getLocation();
+        map.setCenterX(playerLoc.getBlockX());
+        map.setCenterZ(playerLoc.getBlockZ());
+        map.setWorld(playerLoc.getWorld());
+    }
+
+    private Player getPlayerHoldingMap(final MapView map) {
+        return Bukkit.getOnlinePlayers().stream()
+                .filter(player -> {
+                    ItemStack itemInHand = player.getInventory().getItemInMainHand();
+                    return isMapMaterial(itemInHand.getType()) && isMatchingMap(itemInHand.getItemMeta(), map);
+                })
+                .findFirst()
+                .orElse(null);
+    }
+
+    private boolean isMapMaterial(final Material type) {
+        return type == Material.MAP || type == Material.FILLED_MAP;
+    }
+
+    private boolean isMatchingMap(ItemMeta meta, MapView map) {
+        MapMeta mapMeta = (MapMeta) meta;
+        return mapMeta != null
+                && mapMeta.getMapView() != null
+                && mapMeta.getMapView().getId() == map.getId();
     }
 }
