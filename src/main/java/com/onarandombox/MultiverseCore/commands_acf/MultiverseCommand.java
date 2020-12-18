1b65a91de419b959d9e26098da66e35c84066739
benwoo1110 <30431861+benwoo1110@users.noreply.github.com>
2020-12-18 18:35:59 +0800

    Make MultiverseCommandAbstract

--- src/main/java/com/onarandombox/MultiverseCore/commands_acf/MultiverseCommand.java
+++ src/main/java/com/onarandombox/MultiverseCore/commands_acf/MultiverseCommand.java
@@ -3,7 +3,7 @@ package com.onarandombox.MultiverseCore.commands_acf;
 import co.aikar.commands.BaseCommand;
 import com.onarandombox.MultiverseCore.MultiverseCore;
 
-public class MultiverseCommand extends BaseCommand {
+public abstract class MultiverseCommand extends BaseCommand {
 
     protected final MultiverseCore plugin;
     //TODO: Should we put world manager here?
