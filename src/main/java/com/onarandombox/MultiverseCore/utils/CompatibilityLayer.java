20532998930b7444675f6769b9e440406610f294
benwoo1110 <30431861+benwoo1110@users.noreply.github.com>
2021-02-18 14:35:08 +0800

    Add respawn anchor support.

---
+++ src/main/java/com/onarandombox/MultiverseCore/utils/CompatibilityLayer.java
@@ -0,0 +1,40 @@
+package com.onarandombox.MultiverseCore.utils;
+
+import com.dumptruckman.minecraft.util.Logging;
+import org.bukkit.Bukkit;
+import org.bukkit.event.player.PlayerRespawnEvent;
+
+import java.lang.reflect.Method;
+
+public class CompatibilityLayer {
+
+    private static Method checkAnchorSpawn;
+
+    public static void init() {
+        try {
+            checkAnchorSpawn = PlayerRespawnEvent.class.getDeclaredMethod("isAnchorSpawn");
+        } catch (NoSuchMethodException e) {
+            Logging.fine("%s does not support respawn anchors.", Bukkit.getVersion());
+        }
+    }
+
+    /**
+     * Check if the respawn point is of respawn anchor type.
+     * Introduced in minecraft 1.16
+     *
+     * @param event A player respawn event.
+     * @return If the respawn location is an anchor point.
+     */
+    public static boolean isAnchorSpawn(PlayerRespawnEvent event) {
+        if (checkAnchorSpawn == null) {
+            return false;
+        }
+        try {
+            return (boolean) checkAnchorSpawn.invoke(event);
+        } catch (Exception e) {
+            Logging.warning("Error checking for: %s", checkAnchorSpawn);
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
