1c483148dc978365937c620800fa58180dfcf7f8
Jeremy Wood <jwood@aquent.com>
2021-06-04 13:00:04 -0400

    Change default permission for version command. Seriously wth.

--- src/main/java/com/onarandombox/MultiverseCore/commands/VersionCommand.java
+++ src/main/java/com/onarandombox/MultiverseCore/commands/VersionCommand.java
@@ -45,8 +45,11 @@ public class VersionCommand extends MultiverseCommand {
         this.addKey("mvver");
         this.addKey("mvv");
         this.addKey("mvversion");
-        this.setPermission("multiverse.core.version",
-                "Dumps version info to the console, optionally to pastebin.com with -b, to hastebin.com using -h, or to paste.gg with -p.", PermissionDefault.TRUE);
+        this.setPermission(
+                "multiverse.core.version",
+                "Dumps version info to the console, optionally to pastebin.com with -b, to hastebin.com using -h, or to paste.gg with -p.",
+                PermissionDefault.OP)
+        ;
     }
 
     private String getLegacyString() {
