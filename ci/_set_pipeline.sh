34fa336f00e506dc0514a4dd2c7152eb86a02150
Jeremy Wood <git@jeremywood.dev>
2021-08-25 09:40:58 -0400

    Revert "Add pipeline stuff."
    This reverts commit 719ade19f7dda1955fb772a57230c4870e7b8ecc.

--- ci/_set_pipeline.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ "$#" -ne 1 ]; then
-    echo "Unable to parse target. Run fly targets to check for what this could be."
-    exit -1
-fi
-
-fly -t "$1" sp -c ./ci/pipelines/pipeline.yml -p multiverse-core
