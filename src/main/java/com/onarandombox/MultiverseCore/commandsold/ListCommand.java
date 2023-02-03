0117159159ca269ca16590c61519c38339faaa72
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2023-02-03 20:23:53 +0800

    feat: @benwoo1110 Revamp content display API
    commit 1211dfb0574a5d49740b3922147bbcc23540decc
    Author: Ben Woo <30431861+benwoo1110@users.noreply.github.com>
    Date:   Tue Aug 17 16:29:31 2021 +0800
    
        Implement suggested improvements.
    
        * Use singleton pattern for DefaultContentFilter with getInstance method.
        * Have a default SendHandler.
        * Don't need streams for small dataset.
        * Private WorldListContentParser class to improve readability.
    
    commit 562eed82558eadb9107557823ae170f6352f8cb2
    Author: Ben Woo <30431861+benwoo1110@users.noreply.github.com>
    Date:   Tue Aug 17 00:26:57 2021 +0800
    
        Revamp content display API.

--- src/main/java/com/onarandombox/MultiverseCore/commandsold/ListCommand.java
+++ src/main/java/com/onarandombox/MultiverseCore/commandsold/ListCommand.java
@@ -9,11 +9,12 @@ package com.onarandombox.MultiverseCore.commandsold;
 
 import com.onarandombox.MultiverseCore.MultiverseCore;
 import com.onarandombox.MultiverseCore.api.MultiverseWorld;
-import com.onarandombox.MultiverseCore.display.ColorAlternator;
 import com.onarandombox.MultiverseCore.display.ContentDisplay;
-import com.onarandombox.MultiverseCore.display.ContentFilter;
-import com.onarandombox.MultiverseCore.display.DisplayHandlers;
-import com.onarandombox.MultiverseCore.display.settings.PagedDisplaySettings;
+import com.onarandombox.MultiverseCore.display.filters.ContentFilter;
+import com.onarandombox.MultiverseCore.display.filters.DefaultContentFilter;
+import com.onarandombox.MultiverseCore.display.filters.RegexContentFilter;
+import com.onarandombox.MultiverseCore.display.handlers.PagedSendHandler;
+import com.onarandombox.MultiverseCore.display.parsers.ContentParser;
 import org.bukkit.ChatColor;
 import org.bukkit.World;
 import org.bukkit.command.CommandSender;
@@ -21,9 +22,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.permissions.PermissionDefault;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Collection;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * Displays a listing of all worlds that a player can enter.
@@ -43,7 +42,7 @@ public class ListCommand extends MultiverseCommand {
 
     @Override
     public void runCommand(CommandSender sender, List<String> args) {
-        ContentFilter filter = ContentFilter.DEFAULT;
+        ContentFilter filter = DefaultContentFilter.getInstance();
         int page = 1;
 
         // Either page or filter.
@@ -51,13 +50,13 @@ public class ListCommand extends MultiverseCommand {
             try {
                 page = Integer.parseInt(args.get(0));
             } catch (NumberFormatException ignore) {
-                filter = new ContentFilter(args.get(0));
+                filter = RegexContentFilter.fromString(args.get(0));
             }
         }
 
         // Filter then page.
         if (args.size() == 2) {
-            filter = new ContentFilter(args.get(0));
+            filter = RegexContentFilter.fromString(args.get(0));
             try {
                 page = Integer.parseInt(args.get(1));
             } catch (NumberFormatException ignore) {
@@ -65,55 +64,57 @@ public class ListCommand extends MultiverseCommand {
             }
         }
 
-        ContentDisplay.forContent(getListContents(sender))
-                .header("%s====[ Multiverse World List ]====", ChatColor.GOLD)
-                .displayHandler(DisplayHandlers.PAGE_LIST)
-                .colorTool(ColorAlternator.with(ChatColor.AQUA, ChatColor.GOLD))
-                .filter(filter)
-                .setting(PagedDisplaySettings.SHOW_PAGE, page)
-                .show(sender);
+        ContentDisplay.create()
+                .addContentParser(new WorldListContentParser())
+                .withSendHandler(PagedSendHandler.create()
+                        .withHeader("%s====[ Multiverse World List ]====", ChatColor.GOLD)
+                        .withFilter(filter)
+                        .withTargetPage(page))
+                .send(sender);
     }
 
-    private Collection<String> getListContents(@NotNull CommandSender sender) {
-        Player player = (sender instanceof Player) ? (Player) sender : null;
+    private class WorldListContentParser implements ContentParser {
 
-        List<String> worldList = this.plugin.getMVWorldManager().getMVWorlds().stream()
-                .filter(world -> player == null || plugin.getMVPerms().canEnterWorld(player, world))
-                .filter(world -> canSeeWorld(player, world))
-                .map(world -> hiddenText(world) + world.getColoredWorldString() + " - " + parseColouredEnvironment(world.getEnvironment()))
-                .collect(Collectors.toList());
+        @Override
+        public void parse(@NotNull CommandSender sender, @NotNull List<String> content) {
+            Player player = (sender instanceof Player) ? (Player) sender : null;
 
-        this.plugin.getMVWorldManager().getUnloadedWorlds().stream()
-                .filter(world -> plugin.getMVPerms().hasPermission(sender, "multiverse.access." + world, true))
-                .map(world -> ChatColor.GRAY + world + " - UNLOADED")
-                .forEach(worldList::add);
-        
-        return worldList;
-    }
+            plugin.getMVWorldManager().getMVWorlds().stream()
+                    .filter(world -> player == null || plugin.getMVPerms().canEnterWorld(player, world))
+                    .filter(world -> canSeeWorld(player, world))
+                    .map(world -> hiddenText(world) + world.getColoredWorldString() + " - " + parseColouredEnvironment(world.getEnvironment()))
+                    .forEach(content::add);
 
-    private boolean canSeeWorld(Player player, MultiverseWorld world) {
-        return !world.isHidden()
-                || player == null
-                || this.plugin.getMVPerms().hasPermission(player, "multiverse.core.modify", true);
-    }
-
-    private String hiddenText(MultiverseWorld world) {
-        return (world.isHidden()) ? String.format("%s[H] ", ChatColor.GRAY) : "";
-    }
-
-    private String parseColouredEnvironment(World.Environment env) {
-        ChatColor color = ChatColor.GOLD;
-        switch (env) {
-            case NETHER:
-                color = ChatColor.RED;
-                break;
-            case NORMAL:
-                color = ChatColor.GREEN;
-                break;
-            case THE_END:
-                color = ChatColor.AQUA;
-                break;
+            plugin.getMVWorldManager().getUnloadedWorlds().stream()
+                    .filter(world -> plugin.getMVPerms().hasPermission(sender, "multiverse.access." + world, true))
+                    .map(world -> ChatColor.GRAY + world + " - UNLOADED")
+                    .forEach(content::add);
         }
-        return color + env.toString();
+
+        private boolean canSeeWorld(Player player, MultiverseWorld world) {
+            return !world.isHidden()
+                    || player == null
+                    || plugin.getMVPerms().hasPermission(player, "multiverse.core.modify", true);
+        }
+
+        private String hiddenText(MultiverseWorld world) {
+            return (world.isHidden()) ? String.format("%s[H] ", ChatColor.GRAY) : "";
+        }
+
+        private String parseColouredEnvironment(World.Environment env) {
+            ChatColor color = ChatColor.GOLD;
+            switch (env) {
+                case NETHER:
+                    color = ChatColor.RED;
+                    break;
+                case NORMAL:
+                    color = ChatColor.GREEN;
+                    break;
+                case THE_END:
+                    color = ChatColor.AQUA;
+                    break;
+            }
+            return color + env.toString();
+        }
     }
 }
