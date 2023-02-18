974afb4751be01d1266c015fa1b003e13e072905
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2023-02-18 14:19:25 +0800

    feat: Implement help command

---
+++ src/main/java/com/onarandombox/MultiverseCore/commands/UsageCommand.java
@@ -0,0 +1,33 @@
+package com.onarandombox.MultiverseCore.commands;
+
+import co.aikar.commands.CommandHelp;
+import co.aikar.commands.annotation.CommandAlias;
+import co.aikar.commands.annotation.CommandCompletion;
+import co.aikar.commands.annotation.CommandPermission;
+import co.aikar.commands.annotation.Description;
+import co.aikar.commands.annotation.HelpCommand;
+import co.aikar.commands.annotation.Subcommand;
+import co.aikar.commands.annotation.Syntax;
+import com.onarandombox.MultiverseCore.MultiverseCore;
+import org.jetbrains.annotations.NotNull;
+
+@CommandAlias("mv")
+public class UsageCommand extends MultiverseCoreCommand {
+    public UsageCommand(@NotNull MultiverseCore plugin) {
+        super(plugin);
+    }
+
+    @HelpCommand
+    @Subcommand("help")
+    @CommandPermission("multiverse.core.help")
+    @CommandCompletion("@commands:mv")
+    @Syntax("[filter] [page]")
+    @Description("Show Multiverse-Core Command usage.")
+    public void onUsageCommand(CommandHelp help) {
+        if (help.getIssuer().isPlayer()) {
+            // Prevent flooding the chat
+            help.setPerPage(4);
+        }
+        this.plugin.getMVCommandManager().showUsage(help);
+    }
+}
