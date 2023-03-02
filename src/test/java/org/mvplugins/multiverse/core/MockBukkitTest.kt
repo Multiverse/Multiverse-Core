eebb4a4a6d950d8e2289296eb56be97c6d627014
Jeremy Wood <jeremy.wood@7factor.io>
2023-03-02 02:22:47 -0500

    Implement initial test using MockBukkit.

---
+++ src/test/java/org/mvplugins/multiverse/core/MockBukkitTest.kt
@@ -0,0 +1,33 @@
+package org.mvplugins.multiverse.core
+
+import be.seeseemelk.mockbukkit.MockBukkit
+import com.onarandombox.MultiverseCore.MultiverseCore
+import com.onarandombox.MultiverseCore.utils.TestingMode
+import org.bukkit.Server
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertNotNull
+
+class MockBukkitTest {
+
+    lateinit var server: Server
+    lateinit var plugin: MultiverseCore
+
+    @BeforeTest
+    fun setUp() {
+        TestingMode.enable()
+        server = MockBukkit.mock()
+        plugin = MockBukkit.load(MultiverseCore::class.java)
+    }
+
+    @Test
+    fun test() {
+        assertNotNull(plugin)
+    }
+
+    @AfterTest
+    fun tearDown() {
+        MockBukkit.unmock()
+    }
+}
