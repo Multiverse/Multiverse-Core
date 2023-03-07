7ad0b0478717154f159eb0f681db3f7c16f522eb
Jeremy Wood <jeremy.wood@7factor.io>
2023-03-07 00:20:26 -0500

    Abstract TestWithMockBukkit out of MockBukkitTest.

---
+++ src/test/java/org/mvplugins/multiverse/core/TestWithMockBukkit.kt
@@ -0,0 +1,29 @@
+package org.mvplugins.multiverse.core
+
+import be.seeseemelk.mockbukkit.MockBukkit
+import be.seeseemelk.mockbukkit.ServerMock
+import com.onarandombox.MultiverseCore.MultiverseCore
+import com.onarandombox.MultiverseCore.utils.TestingMode
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+
+/**
+ * Basic abstract test class that sets up MockBukkit and MultiverseCore.
+ */
+abstract class TestWithMockBukkit {
+
+    protected lateinit var server: ServerMock
+    protected lateinit var multiverseCore: MultiverseCore
+
+    @BeforeTest
+    fun setUp() {
+        TestingMode.enable()
+        server = MockBukkit.mock()
+        multiverseCore = MockBukkit.load(MultiverseCore::class.java)
+    }
+
+    @AfterTest
+    fun tearDown() {
+        MockBukkit.unmock()
+    }
+}
