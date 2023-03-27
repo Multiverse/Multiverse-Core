5a0957d894989d53c611803b59bad3276a5aafd4
Jeremy Wood <jeremy.wood@7factor.io>
2023-03-27 00:51:55 -0400

    Remove unused MultiverseConfigurationException.

--- src/main/java/com/onarandombox/MultiverseCore/config/MultiverseConfigurationException.java
+++
@@ -1,9 +0,0 @@
-package com.onarandombox.MultiverseCore.config;
-
-import java.io.IOException;
-
-public class MultiverseConfigurationException extends IOException {
-    public MultiverseConfigurationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
