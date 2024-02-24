7276da766514020af73bdd6a2e420b082533f06d
Zax71 <zabax32@gmail.com>
2024-02-24 13:48:57 +0000

    Implement i18n

--- src/main/java/org/mvplugins/multiverse/core/commands/VersionCommand.java
+++ src/main/java/org/mvplugins/multiverse/core/commands/VersionCommand.java
@@ -1,17 +1,18 @@
 package org.mvplugins.multiverse.core.commands;
 
 import co.aikar.commands.BukkitCommandIssuer;
+import co.aikar.commands.MessageType;
 import co.aikar.commands.annotation.CommandAlias;
 import co.aikar.commands.annotation.CommandPermission;
 import co.aikar.commands.annotation.Description;
 import co.aikar.commands.annotation.Subcommand;
 import jakarta.inject.Inject;
-import org.bukkit.ChatColor;
 import org.jetbrains.annotations.NotNull;
 import org.jvnet.hk2.annotations.Service;
 import org.mvplugins.multiverse.core.MultiverseCore;
 import org.mvplugins.multiverse.core.commandtools.MVCommandManager;
 import org.mvplugins.multiverse.core.commandtools.MultiverseCommand;
+import org.mvplugins.multiverse.core.utils.MVCorei18n;
 
 @Service
 @CommandAlias("mv")
@@ -29,8 +30,8 @@ class VersionCommand extends MultiverseCommand {
     @CommandPermission("multiverse.core.version")
     @Description("{@@mv-core.version.description}")
     void versionCommand(BukkitCommandIssuer issuer) {
-        issuer.sendMessage("Multiverse Core version: " + ChatColor.WHITE + plugin.getDescription().getVersion());
-        issuer.sendMessage("Multiverse Core authors: " + ChatColor.WHITE + String.join(", ", plugin.getDescription().getAuthors()));
-        issuer.sendMessage("Special Code: " + ChatColor.WHITE + "FRN002"); // An in joke I don't get...
+        issuer.sendMessage(MessageType.INFO, MVCorei18n.VERSION_MV, "{version}", plugin.getDescription().getVersion());
+        issuer.sendMessage(MessageType.INFO, MVCorei18n.VERSION_AUTHORS, "{authors}", String.join(", ", plugin.getDescription().getAuthors()));
+        issuer.sendMessage(MessageType.INFO, MVCorei18n.VERSION_SECRETCODE); // An in joke I don't get...
     }
 }
