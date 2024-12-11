80ad78802857aedea6f416c905105a8a8aacf638
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2024-12-11 17:23:19 +0800

    Abstract common action for command testing to a base class

---
+++ src/test/java/org/mvplugins/multiverse/core/commands/BaseCommandTest.kt
@@ -0,0 +1,44 @@
+package org.mvplugins.multiverse.core.commands
+
+import org.bukkit.ChatColor
+import org.bukkit.command.ConsoleCommandSender
+import org.bukkit.permissions.PermissionAttachment
+import org.mockbukkit.mockbukkit.entity.PlayerMock
+import org.mvplugins.multiverse.core.TestWithMockBukkit
+import org.mvplugins.multiverse.core.commandtools.PluginLocales
+import org.mvplugins.multiverse.core.utils.message.Message
+import kotlin.test.BeforeTest
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+
+abstract class BaseCommandTest : TestWithMockBukkit() {
+
+    protected lateinit var player: PlayerMock
+    protected lateinit var console: ConsoleCommandSender
+
+    private lateinit var locales : PluginLocales
+    private lateinit var attachment : PermissionAttachment
+
+    @BeforeTest
+    fun setUpCommand() {
+        locales = serviceLocator.getActiveService(PluginLocales::class.java).takeIf { it != null } ?: run {
+            throw IllegalStateException("PluginLocales is not available as a service") }
+
+        console = server.consoleSender
+        player = server.addPlayer("benwoo1110");
+        assertEquals(player, server.getPlayer("benwoo1110"))
+        attachment = player.addAttachment(multiverseCore)
+        assertNotNull(attachment)
+    }
+
+    fun addPermission(permission: String) {
+        attachment.setPermission(permission, true)
+    }
+
+    fun assertCommandOutput(message : Message) {
+        assertEquals(
+            ChatColor.stripColor(ChatColor.translateAlternateColorCodes('&',message.formatted(locales))),
+            ChatColor.stripColor(player.nextMessage())
+        )
+    }
+}
