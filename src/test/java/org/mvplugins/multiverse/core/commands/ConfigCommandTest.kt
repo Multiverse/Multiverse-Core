0700d481d12502ae802397bd06ea3038f57cf713
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2024-12-11 17:23:34 +0800

    Add test for config command

---
+++ src/test/java/org/mvplugins/multiverse/core/commands/ConfigCommandTest.kt
@@ -0,0 +1,51 @@
+package org.mvplugins.multiverse.core.commands
+
+import co.aikar.commands.MessageKeys
+import org.bukkit.Bukkit
+import org.junit.jupiter.api.Test
+import org.mvplugins.multiverse.core.config.MVCoreConfig
+import org.mvplugins.multiverse.core.utils.MVCorei18n
+import org.mvplugins.multiverse.core.utils.message.Message
+import org.mvplugins.multiverse.core.utils.message.MessageReplacement.replace
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class ConfigCommandTest : BaseCommandTest() {
+
+    @Test
+    fun `Modify config global-debug`() {
+        assertTrue(Bukkit.dispatchCommand(console, "mv config global-debug 2"))
+        val coreConfig = serviceLocator.getActiveService(MVCoreConfig::class.java).takeIf { it != null } ?: run {
+            throw IllegalStateException("MVCoreConfig is not available as a service") }
+        assertEquals(2, coreConfig.globalDebug)
+    }
+
+    @Test
+    fun `Modify config global-debug player no permission`() {
+        assertTrue(Bukkit.dispatchCommand(player, "mv config global-debug 2"))
+        assertCommandOutput(Message.of(MessageKeys.PERMISSION_DENIED, ""))
+
+        val coreConfig = serviceLocator.getActiveService(MVCoreConfig::class.java).takeIf { it != null } ?: run {
+            throw IllegalStateException("MVCoreConfig is not available as a service") }
+        assertEquals(0, coreConfig.globalDebug)
+    }
+
+    @Test
+    fun `Modify non-existing config property`() {
+        addPermission("multiverse.core.config")
+        assertTrue(Bukkit.dispatchCommand(player, "mv config invalid-property test"))
+        player.nextMessage() // ignore the first line
+        assertCommandOutput(Message.of(
+            MVCorei18n.CONFIG_NODE_NOTFOUND,
+            "",
+            replace("{node}").with("invalid-property")))
+    }
+
+    @Test
+    fun `Modify config global-debug invalid type`() {
+        addPermission("multiverse.core.config")
+        assertTrue(Bukkit.dispatchCommand(player, "mv config global-debug what"))
+        player.nextMessage() // ignore the first line
+        assertCommandOutput(Message.of("Unable to convert 'what' to number. (integer)")) // todo: localize
+    }
+}
