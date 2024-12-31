f20b4f285fc88ee426026d6586fe31818b6f7036
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2024-12-31 11:27:06 +0800

    Bump mockbukkit with canGenerateStructures method support

--- src/test/java/org/mvplugins/multiverse/core/mock/MVWorldMock.java
+++ src/test/java/org/mvplugins/multiverse/core/mock/MVWorldMock.java
@@ -10,21 +10,14 @@ import java.io.File;
 public class MVWorldMock extends WorldMock {
 
     private final File worldFolder;
-    private final boolean generateStructures;
 
     public MVWorldMock(@NotNull WorldCreator creator) {
         super(creator);
         this.worldFolder = new File(MockBukkit.getMock().getWorldContainer(), getName());
-        this.generateStructures = creator.generateStructures();
     }
 
     @Override
     public @NotNull File getWorldFolder() {
         return this.worldFolder;
-    }
-
-    @Override
-    public boolean canGenerateStructures() {
-        return this.generateStructures;
     }
 }
