12c99114340a204e9971b107cc7bfcded3e77d0d
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2025-01-24 21:52:49 +0800

    Convert GameRuleValue class to a record

--- src/main/java/org/mvplugins/multiverse/core/commandtools/context/GameRuleValue.java
+++ src/main/java/org/mvplugins/multiverse/core/commandtools/context/GameRuleValue.java
@@ -3,14 +3,5 @@ package org.mvplugins.multiverse.core.commandtools.context;
 /**
  * Simple wrapper for game rule value, as they may be different types.
  */
-public final class GameRuleValue {
-    private final Object value;
-
-    public GameRuleValue(Object value) {
-        this.value = value;
-    }
-
-    public Object getValue() {
-        return value;
-    }
+public record GameRuleValue(Object value) {
 }
