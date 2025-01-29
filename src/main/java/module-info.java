6e70b252a62e4a061aadcddc70b69193533da4ad
Jeremy Wood <jeremy.wood@7factor.io>
2025-01-29 09:49:15 -0500

    Set up Java module.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,21 @@
+module multiverse.core.main {
+    requires acf.paper;
+    requires bstats.base;
+    requires bstats.bukkit;
+    requires com.google.common;
+    requires CommentedConfiguration;
+    requires commons.lang;
+    requires io.vavr;
+    requires jakarta.annotation;
+    requires jakarta.inject;
+    requires java.logging;
+    requires json.smart;
+    requires Logging;
+    requires mappings;
+    requires org.bukkit;
+    requires org.glassfish.hk2.api;
+    requires org.jetbrains.annotations;
+    requires paperlib;
+    requires placeholderapi;
+    requires VaultAPI;
+}
