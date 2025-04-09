677dd475f7a9ae99b022423f11779df54e2ed44a
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2025-04-09 10:15:40 +0800

    Improve entity spawn config info command output

--- src/main/java/org/mvplugins/multiverse/core/commands/EntitySpawnConfigCommand.java
+++ src/main/java/org/mvplugins/multiverse/core/commands/EntitySpawnConfigCommand.java
@@ -9,37 +9,77 @@ import co.aikar.commands.annotation.Subcommand;
 import co.aikar.commands.annotation.Syntax;
 import jakarta.inject.Inject;
 import org.bukkit.entity.SpawnCategory;
+import org.jetbrains.annotations.NotNull;
 import org.jvnet.hk2.annotations.Optional;
 import org.jvnet.hk2.annotations.Service;
 import org.mvplugins.multiverse.core.command.MVCommandIssuer;
+import org.mvplugins.multiverse.core.command.flag.ParsedCommandFlags;
+import org.mvplugins.multiverse.core.command.flags.PageFilterFlags;
 import org.mvplugins.multiverse.core.config.handle.PropertyModifyAction;
+import org.mvplugins.multiverse.core.display.ContentDisplay;
+import org.mvplugins.multiverse.core.display.filters.DefaultContentFilter;
+import org.mvplugins.multiverse.core.display.handlers.PagedSendHandler;
+import org.mvplugins.multiverse.core.display.parsers.ListContentProvider;
+import org.mvplugins.multiverse.core.locale.message.Message;
+import org.mvplugins.multiverse.core.utils.StringFormatter;
 import org.mvplugins.multiverse.core.world.MultiverseWorld;
 import org.mvplugins.multiverse.core.world.WorldManager;
+import org.mvplugins.multiverse.core.world.entity.SpawnCategoryConfig;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 @Subcommand("entity-spawn-config")
 final class EntitySpawnConfigCommand extends CoreCommand {
 
     private final WorldManager worldManager;
+    private final PageFilterFlags flags;
 
     @Inject
-    EntitySpawnConfigCommand(WorldManager worldManager) {
+    EntitySpawnConfigCommand(@NotNull WorldManager worldManager, @NotNull PageFilterFlags flags) {
         this.worldManager = worldManager;
+        this.flags = flags;
     }
 
     @Subcommand("info")
     @CommandPermission("multiverse.core.mobsspawnconfig")
-    @CommandCompletion("@mvworlds:scope=both")
-    @Syntax("[world]")
+    @CommandCompletion("@mvworlds:scope=both|@flags:resolveUntil=arg1,groupName=" + PageFilterFlags.NAME + " @flags:groupName=" + PageFilterFlags.NAME)
+    @Syntax("[world] [--page <page>] [--filter <filter>]")
     @Description("")
     void onInfoCommand(
             MVCommandIssuer issuer,
 
             @Flags("resolve=issuerAware")
             @Syntax("[world]")
-            MultiverseWorld world
+            MultiverseWorld world,
+
+            @Optional
+            @Syntax("[--page <page>] [--filter <filter>]")
+            String[] flagArray
     ) {
-        issuer.sendMessage(world.getMobsSpawnConfig().toString());
+        ParsedCommandFlags parsedFlags = flags.parse(flagArray);
+        ContentDisplay.create()
+                .addContent(ListContentProvider.forContent(getEntitySpawnConfigList(world)))
+                .withSendHandler(PagedSendHandler.create()
+                        .withHeader(Message.of("==== [ Entity Spawn Config '" + world.getName() + "' ] ===="))
+                        .withLinesPerPage(8)
+                        .withTargetPage(parsedFlags.flagValue(flags.page, 1))
+                        .withFilter(parsedFlags.flagValue(flags.filter, DefaultContentFilter.get())))
+                .send(issuer);
+    }
+
+    private List<String> getEntitySpawnConfigList(MultiverseWorld world) {
+        List<String> list = new ArrayList<>();
+        Arrays.stream(SpawnCategory.values()).forEach(spawnCategory -> {
+            list.add(spawnCategory.name() + ": ");
+            SpawnCategoryConfig spawnCategoryConfig = world.getMobsSpawnConfig().getSpawnCategoryConfig(spawnCategory);
+            list.add("  spawn: " + spawnCategoryConfig.isSpawn());
+            list.add("  tick-rate: " + spawnCategoryConfig.getTickRate());
+            list.add("  exceptions: " + StringFormatter.join(spawnCategoryConfig.getExceptions(), ", "));
+        });
+        return list;
     }
 
     @Subcommand("modify")
