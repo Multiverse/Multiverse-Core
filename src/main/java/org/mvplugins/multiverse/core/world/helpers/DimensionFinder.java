468399ad84c7f2ec1a231dc360634df42ff5d912
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2025-05-03 19:14:12 +0800

    Add javadoc for DimensionFinder

--- src/main/java/org/mvplugins/multiverse/core/world/helpers/DimensionFinder.java
+++ src/main/java/org/mvplugins/multiverse/core/world/helpers/DimensionFinder.java
@@ -13,6 +13,9 @@ import org.mvplugins.multiverse.core.world.WorldManager;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * Helps to find the overworld world from a nether or end world and vice versa.
+ */
 @Service
 public final class DimensionFinder {
 
@@ -25,18 +28,42 @@ public final class DimensionFinder {
         this.worldManager = worldManager;
     }
 
+    /**
+     * Gets whether a world is the overworld, i.e. the environment is NORMAL.
+     *
+     * @param world target world to check
+     * @return true if the world is an overworld
+     */
     public boolean isOverworld(MultiverseWorld world) {
         return world.getEnvironment() == Environment.NORMAL;
     }
 
+    /**
+     * Gets whether a world is the nether, i.e. the environment is NETHER.
+     *
+     * @param world target world to check
+     * @return true if the world is a nether
+     */
     public boolean isNether(MultiverseWorld world) {
         return world.getEnvironment() == Environment.NETHER;
     }
 
+    /**
+     * Gets whether a world is the end, i.e. the environment is THE_END
+     *
+     * @param world target world to check
+     * @return true if the world is an end
+     */
     public boolean isEnd(MultiverseWorld world) {
         return world.getEnvironment() == Environment.THE_END;
     }
 
+    /**
+     * Gets the Multiverse overworld world from a nether or end world
+     *
+     * @param world target world
+     * @return The overworld multiverse world if exist
+     */
     public Option<MultiverseWorld> getOverworldWorld(MultiverseWorld world) {
         if (isOverworld(world)) {
             return Option.of(world);
@@ -50,6 +77,12 @@ public final class DimensionFinder {
         return Option.none();
     }
 
+    /**
+     * Gets the Multiverse nether world from an overworld or end world
+     *
+     * @param world target world
+     * @return The nether multiverse world if exist
+     */
     public Option<MultiverseWorld> getNetherWorld(MultiverseWorld world) {
         if (isOverworld(world)) {
             return worldManager.getWorld(getNetherNameFromOverworld(world.getName()));
@@ -65,6 +98,12 @@ public final class DimensionFinder {
         return Option.none();
     }
 
+    /**
+     * Gets the Multiverse end world from an overworld or nether world
+     *
+     * @param world target world
+     * @return The end multiverse world if exist
+     */
     public Option<MultiverseWorld> getEndWorld(MultiverseWorld world) {
         if (isOverworld(world)) {
             return worldManager.getWorld(getEndNameFromOverworld(world.getName()));
@@ -96,6 +135,9 @@ public final class DimensionFinder {
         return config.getEndWorldNameFormat().replaceOverworld(worldName);
     }
 
+    /**
+     * Represents a dimension name format, used to extract the overworld world name from a nether or end world name.
+     */
     public static final class DimensionFormat {
 
         public static final String OVERWORLD_PLACEHOLDER = "%overworld%";
@@ -111,6 +153,12 @@ public final class DimensionFinder {
             this.pattern = Pattern.compile("^" + format.replace(OVERWORLD_PLACEHOLDER, "(?<worldname>.+)") + "$");
         }
 
+        /**
+         * Extract the overworld world name from a nether or end world name
+         *
+         * @param worldName The nether or end world name
+         * @return The overworld world name
+         */
         public Option<String> getOverworldFromName(String worldName) {
             Matcher matcher = pattern.matcher(worldName);
             if (!matcher.matches()) {
@@ -120,10 +168,21 @@ public final class DimensionFinder {
             return Option.of(matcher.group("worldname"));
         }
 
+        /**
+         * Parse the dimension name with the overworld world name
+         *
+         * @param worldName The overworld name
+         * @return The parsed dimension name
+         */
         public String replaceOverworld(String worldName) {
             return format.replace(OVERWORLD_PLACEHOLDER, worldName);
         }
 
+        /**
+         * The format used to format the dimension's name
+         *
+         * @return the format
+         */
         public @NotNull String getFormat() {
             return format;
         }
