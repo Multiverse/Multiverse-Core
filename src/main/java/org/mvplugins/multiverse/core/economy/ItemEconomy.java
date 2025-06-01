913f131b3b7b39d3508fb23c90d00cc63b2a283f
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2025-06-01 20:32:11 +0800

    Cleanup double to int conversion for item economy

--- src/main/java/org/mvplugins/multiverse/core/economy/ItemEconomy.java
+++ src/main/java/org/mvplugins/multiverse/core/economy/ItemEconomy.java
@@ -7,16 +7,20 @@ import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
 import org.jvnet.hk2.annotations.Service;
+import org.mvplugins.multiverse.core.command.MVCommandManager;
 
 @Service
 final class ItemEconomy {
 
     private static final String ECONOMY_NAME = "Simple Item Economy";
+    @NotNull
+    private final MVCommandManager commandManager;
 
     @Inject
-    private ItemEconomy() {
-        // Can't instantiate
+    private ItemEconomy(@NotNull MVCommandManager commandManager) {
+        this.commandManager = commandManager;
     }
 
     String getFormattedPrice(double amount, Material currency) {
@@ -33,7 +37,7 @@ final class ItemEconomy {
 
     boolean hasEnough(Player player, double amount, Material currency) {
         if (currency != null) {
-            return player.getInventory().contains(currency, (int) amount);
+            return player.getInventory().contains(currency, augmentAmount(amount));
         } else {
             return true;
         }
@@ -41,30 +45,30 @@ final class ItemEconomy {
 
     void deposit(Player player, double amount, Material currency) {
         if (MVEconomist.isItemCurrency(currency)) {
-            giveItem(player, amount, currency);
+            giveItem(player, -augmentAmount(amount), currency);
         }
     }
 
     void withdraw(Player player, double amount, Material currency) {
         if (MVEconomist.isItemCurrency(currency)) {
-            takeItem(player, amount, currency);
+            takeItem(player, augmentAmount(amount), currency);
         }
     }
 
-    void giveItem(Player player, double amount, Material type) {
-        ItemStack item = new ItemStack(type, (int) amount);
+    void giveItem(Player player, int amount, Material type) {
+        ItemStack item = new ItemStack(type, amount);
         player.getInventory().addItem(item);
-        showReceipt(player, (amount * -1), type);
+        showReceipt(player, amount, type);
     }
 
-    void takeItem(Player player, double amount, Material type) {
+    void takeItem(Player player, int amount, Material type) {
         int removed = 0;
         HashMap<Integer, ItemStack> items = (HashMap<Integer, ItemStack>) player.getInventory().all(type);
         for (int i : items.keySet()) {
             if (removed >= amount) {
                 break;
             }
-            int diff = (int) (amount - removed);
+            int diff = amount - removed;
             int amt = player.getInventory().getItem(i).getAmount();
             if (amt - diff > 0) {
                 player.getInventory().getItem(i).setAmount(amt - diff);
@@ -77,14 +81,18 @@ final class ItemEconomy {
         showReceipt(player, amount, type);
     }
 
-    void showReceipt(Player player, double price, Material item) {
+    void showReceipt(Player player, int price, Material item) {
         if (price > 0D) {
             player.sendMessage(String.format("%s%s%s %s",
                     ChatColor.WHITE, "You have been charged", ChatColor.GREEN, getFormattedPrice(price, item)));
         } else if (price < 0D) {
             player.sendMessage(String.format("%s%s%s %s",
-                    ChatColor.DARK_GREEN, getFormattedPrice((price * -1), item),
+                    ChatColor.DARK_GREEN, getFormattedPrice(price, item),
                     ChatColor.WHITE, "has been added to your inventory."));
         }
+    }
+
+    private int augmentAmount(@NotNull Double amount) {
+        return amount.intValue();
     }
 }
